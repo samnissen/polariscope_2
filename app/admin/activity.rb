b66ce344beb4927d780fb4979f31a5a48be2092f
Prelang Builder <builder@prelang.com>
2015-09-07 10:37:35 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Activity' to allow for create, update

--- app/admin/activity.rb
+++ app/admin/activity.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Activity do
+  permit_params :action_name
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
