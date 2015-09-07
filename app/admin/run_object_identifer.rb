4b0fe156d4fc1359645b9edc59fd6a8d09d79f04
Prelang Builder <builder@prelang.com>
2015-09-07 10:37:56 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'RunObjectIdentifer' to allow for create, update

--- app/admin/run_object_identifer.rb
+++ app/admin/run_object_identifer.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register RunObjectIdentifer do
+  permit_params :identifier, :id_type, :selector
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
