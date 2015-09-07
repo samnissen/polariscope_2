cbee548fd889fb06267684446e5829f4c4f87ec2
Prelang Builder <builder@prelang.com>
2015-09-07 10:37:30 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'SiblingRelationship' to allow for create, update

--- app/admin/sibling_relationship.rb
+++ app/admin/sibling_relationship.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register SiblingRelationship do
+  permit_params :relation
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
