ddc851ea1595c875f2b725d421769a5fce511001
Prelang Builder <builder@prelang.com>
2015-09-07 10:37:12 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'DataElement' to allow for create, update

--- app/admin/data_element.rb
+++ app/admin/data_element.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register DataElement do
+  permit_params :key, :value
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
