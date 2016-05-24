90588a0efa7a554504c28db85861d25def047b02
jhulme <jhulme81@gmail.com>
2016-05-24 17:17:02 +0100

    Change name of rta id parameter passed through to controller from id in order to avoid incorrectly pulling through run rather than run test action.

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -73,7 +73,7 @@ class RunsController < ApplicationController
   end
 
   def update_action_status
-    statusbtn_id = params[:id]
+    statusbtn_id = params[:status_action_id]
     statusbtn_id = /\d+/.match(statusbtn_id) #strip the rta id from the status button identifier
     rta = RunTestAction.find(statusbtn_id.to_s) #had to convert to string value for find to work
     render :partial => "runs/status_icon", :locals => { :rta => rta }
