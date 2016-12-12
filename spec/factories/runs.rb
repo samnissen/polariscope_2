4ad4d73a25d2324c37384abdcfe5df01db8e6e29
Sam Nissen <scnissen@gmail.com>
2016-12-12 12:34:17 +0000

    Display all browser types by unscoping helper calls

--- spec/factories/runs.rb
+++ spec/factories/runs.rb
@@ -4,12 +4,24 @@ FactoryGirl.define do
     description "MyString"
     collection
     environment
-    browsers { FactoryGirl.create_list(:browser_type, 2).map{|bt| "#{bt.key}".to_sym}.compact }
+    browsers { FactoryGirl.create_list(:browser_type, 3).map{|bt| "#{bt.key}".to_sym}.compact }
+    test_ids [1,2,3]
 
     after(:build) { |run| run.class.skip_callback(:create, :before, :compile) }
 
     factory :run_with_compile do
-      after(:build) { |run| run.send(:compile) }
+      before(:create) do |run|
+        testset1 = create(:testset, {:collection => run.collection})
+        testset2 = create(:testset, {:collection => run.collection})
+        run.test_ids = [testset1.id, testset2.id]
+        ts1_test_actions = create_list(:test_action, 3, {testset: testset1})
+        ts2_test_actions = create_list(:test_action, 3, {testset: testset2})
+        (ts1_test_actions + ts2_test_actions).each do |ta|
+          create(:object_identifier, {:test_action => ta})
+        end
+      end
+
+      before(:create) { |run| run.send(:compile) }
     end
   end
 
--- spec/helpers/runs_helper_spec.rb
+++ spec/helpers/runs_helper_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+include ERB::Util # https://github.com/rails/rails/issues/7330
 
 # Specs in this file have access to a helper object that includes
 # the RunsHelper. For example:
@@ -11,5 +12,33 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe RunsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "a run with an archived browser" do
+    before(:all) do
+      @run = create(:run_with_compile)
+      @archived_browser = BrowserType.where(:key => @run.browsers.last).last
+      @another_browser = BrowserType.where(:key => @run.browsers.first).last
+      @archived_browser.update_attribute(:archived, true)
+    end
+
+    it "displays all browsers" do
+      browser_displayed = display_browsers(@run)
+
+      expect(browser_displayed).to match(Regexp.new("#{@archived_browser.name}"))
+      expect(browser_displayed).to match(Regexp.new("#{@another_browser.name}"))
+    end
+
+    it "gets link to all test statuses with name" do
+      result = display_test_status(@run.run_tests.first)
+
+      expect(result).to match(Regexp.new("#{@archived_browser.name}"))
+      expect(result).to match(Regexp.new("#{@another_browser.name}"))
+    end
+
+    it "gets link to all test actions with name"  do
+      result = display_test_action_status(@run.run_tests.first.run_test_actions.first)
+
+      expect(result).to match(Regexp.new("#{@archived_browser.name}"))
+      expect(result).to match(Regexp.new("#{@another_browser.name}"))
+    end
+  end
 end
